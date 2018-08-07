9ff9e5ebd72d19dcd639f3658a9c3ec15f65bc18
Zae <526131722@qq.com>
2018-08-08 00:32:43 +0800

    添加样式自定义

--- calendar2excel.py
+++ calendar2excel.py
@@ -8,6 +8,30 @@ options = {
     "day-cols": 2  # 每日占列数 ≥ 1
 }
 
+styles = {
+    # 头部单元格样式
+    "header": {"align":"center", "bg_color":"#A6A6A6"},
+
+    # 针对每日整个区块的样式 允许以list形式轮流变换样式
+    "day-block": [{"bg_color":"#C8C8C8"},{"bg_color":"#D9D9D9"}],
+
+    # 每日头部部分的样式, 重复项会覆盖day-block
+    "day-header": {"align":"left"},   
+
+    # 每日内容部分的样式, 重复项会覆盖day-block
+    "day-text": {},
+
+    # 非本月部分的样式
+    "blank-day": {"align":"center", "valign":"vcenter", "bg_color":"#EAEAEA"}, 
+}
+
+# styles["day-block"] = [{"bg_color":"#C8C8C8"}]*5+[{"bg_color":"#D9D9D9"}]*2
+# styles["blank-day"] = {"align":"center", "valign":"vcenter", "bg_color":"#EAEAEA"}
+
+# styles["header"]["font_size"] = 12
+# styles["day-header"]["font_size"] = 12
+# styles["day-text"]["italic"] = True
+
 cells_format = {}
 cells_value = {}
 
@@ -63,19 +87,22 @@ def generate(year, month, filename):
     weekdays_title = weekdays_title[fwd:] + weekdays_title[:fwd]
     for x in range(7):
         write_value(0, x * span, weekdays_title[x])
-        write_format(0, x * span, center)
+        write_format(0, x * span, styles["header"])
 
     for w in range(len(weekdays)):
         for d in range(7):
             y, x = 1 + w * options["day-rows"], d * span
+            write_formats(y, x, y+options["day-rows"]-1, x+options["day-cols"]-1, styles["day-block"][(w*7+d)%len(styles["day-block"])])
             if weekdays[w][d] == 0:
-                ws.merge_range(y, x, y+options["day-rows"]-1, x+options["day-cols"]-1, None, )
-                write_format(y, x, {"valign":"vcenter"})
+                ws.merge_range(y, x, y+options["day-rows"]-1, x+options["day-cols"]-1, None)
+                write_format(y, x, styles["blank-day"])
             else:
                 write_value(y, x, str(weekdays[w][d]) + "日")
+                write_format(y, x, styles["day-header"])
                 for i in range(1, options["day-rows"]):
                     ws.merge_range(y+i, x, y+i, x+options["day-cols"]-1, None)
-            write_format(y, x, center)
+                    write_formats(y+i, x, y+i, x+options["day-cols"]-1, styles["day-text"])
+            
 
     border_top = {"top": 1}
     border_bottom = {"bottom": 1}
--- readme.md
+++ readme.md
@@ -1,14 +1,21 @@
-### Calendar2Excel - 生成空Excel日历
+## Calendar2Excel - 生成空Excel日历
 
-![preview](https://user-images.githubusercontent.com/12966814/43713228-6d809016-99ab-11e8-9408-fc262251d24e.png)
+![preview](https://user-images.githubusercontent.com/12966814/43789296-29b8bf32-9aa2-11e8-89df-a1dc66b03bcb.PNG)
 
-#### 环境
+这是一个方便生成Excel日历的脚本, 提供了大量可自定义的内容, 你可以按照自己需求修改参数并生成不同样式的日历.
+
+- 自定义生成何年何月的日历
+- 自定义每日跨列/行数
+- 自定义每周开始日
+- 自定义各单元格样式, 包括背景色, 字体, 对齐等
+
+### 环境
 
 Python 3.6
 
 xlsxwriter
 
-#### 自定义
+### 自定义
 
 修改`options`参数控制表格生成:
 
@@ -28,5 +35,44 @@ if __name__ == '__main__':
     filename = "%d-%02d.xlsx" % (today.year, today.month)
     generate(today.year, today.month, filename) # 生成当前月 输出YYYY-MM.xlsx
 
+```
+
+#### 修改样式
+
+修改`styles`参数控制输出的表格样式:
+
+```python
+styles = {
+    # 头部单元格样式
+    "header": {"align":"center", "bg_color":"#A6A6A6"},
+
+    # 针对每日整个区块的样式 允许以list形式轮流变换样式
+    "day-block": [{"bg_color":"#C8C8C8"},{"bg_color":"#D9D9D9"}],
+
+    # 每日头部部分的样式, 重复项会覆盖day-block
+    "day-header": {"align":"left"},   
+
+    # 每日内容部分的样式, 重复项会覆盖day-block
+    "day-text": {},
+
+    # 非本月部分的样式
+    "blank-day": {"align":"center", "valign":"vcenter", "bg_color":"#EAEAEA"}, 
+}
+```
+样式参考请参照: [XlsxWriter-Format方法和属性](https://xlsxwriter.readthedocs.io/format.html#format-methods-and-format-properties)
+
+修改示例:
+
+```python
+# 本月每周前5天背景色较深
+styles["day-block"] = [{"bg_color":"#C8C8C8"}]*5+[{"bg_color":"#D9D9D9"}]*2
+styles["blank-day"] = {"align":"center", "valign":"vcenter", "bg_color":"#EAEAEA"}
+```
+
+```python
+# 头部字体设为12(默认11) 每日内容设为斜体
+styles["header"]["font_size"] = 12
+styles["day-header"]["font_size"] = 12
+styles["day-text"]["italic"] = True
 ```
 
